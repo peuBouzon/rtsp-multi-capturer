78df1523b1e8b20fc7d9a8dbbb41c490cd5b825b
peubouzon <peubouzon@gmail.com>
2022-08-06 15:18:07 -0300

    add logging, docstrings and fix bugs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from multicapturer.multicapturer import MultiCapturer
 import cv2 as cv
 import argparse
+import logging
+from datetime import datetime
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -8,15 +10,22 @@ if __name__ == "__main__":
     parser.add_argument('--fps', type=int, default=1)
     args = parser.parse_args()
 
-    multi_capturer = MultiCapturer(conn_strs=args.rtsp, frame_rate=args.fps)
+    logging.basicConfig(filename='multi-camera.log', level=logging.DEBUG)
+    logging.info(f'Application started on {datetime.now()}')
 
+    multi_capturer = MultiCapturer(conn_strs=args.rtsp, frame_rate=args.fps)
+    stop_execution = False
     while True:
         windows = [cv.namedWindow(
-            f'Frame {i}', cv.WINDOW_NORMAL) for i in len(args.rtsp)]
+            f'Frame {i}', cv.WINDOW_NORMAL) for i in range(len(args.rtsp))]
         frames = multi_capturer.queue.get()
         for i, frame in enumerate(frames):
             if (frame.ret):
                 cv.imshow(f'Frame {i}', frame.frame)
             if cv.waitKey(1) & 0xFF == ord('q'):
-                cv.destroyAllWindows()
-                break
+                stop_execution = True
+        if stop_execution:
+            multi_capturer.stop()
+            cv.destroyAllWindows()
+            break
+    logging.info(f'Application finished on {datetime.now()}')
--- multicapturer/multicapturer.py
+++ multicapturer/multicapturer.py
@@ -5,9 +5,43 @@ import threading
 import time
 import queue
 from data.frame import Frame
+import logging
 
 
 class Capturer:
+    """
+    A class used to read a rtsp stream whenever triggered
+
+    ...
+
+    Attributes
+    ----------
+    RECONNECTION_PERIOD: int
+        the period to retry reconnections to the camera.
+    conn_str : str
+        the rtsp connection string
+    max_failures_to_shutdown : int
+        the maximum number of failures to read before try to reconnect
+    thread : threading.Thread
+        the capturer thread
+    retrieve_event : threading.Event
+        the event that triggers the camera reading
+    done_event: threading.Event
+        the event that indicates that a new frame is available
+    stop_event: threading.Event
+        the event that stop the capturer thread execution
+    queue: queue.Queue
+        the queue that stores the new frames
+
+    Methods
+    -------
+    target()
+        The capturer's thread target function. It puts a new frame on the
+        capturer's queue whenever the retrieve_event is set and the result is
+        indicated by the done_event.
+    stop()
+        Stops the capturer's thread.
+    """
     RECONNECTION_PERIOD = 30
 
     def __init__(self, conn_str: str, max_failures_to_shutdown: int) -> None:
@@ -21,13 +55,15 @@ class Capturer:
         self.thread.start()
 
     def target(self):
-        capturer = cv.VideoCapture(self.conn_str)
+        capturer = cv.VideoCapture(
+            self.conn_str if not self.conn_str.isdigit() else int(self.conn_str))
         closed = not capturer.isOpened()
         last_reconnection_try = time.time()
         fails = self.max_failures_to_shutdown if closed else 0
+        ret = capturer.grab()
         while not self.stop_event.is_set():
             if self.retrieve_event.is_set():
-                if closed or not ret:
+                if closed:
                     self.queue.put_nowait(Frame(ret, None, self.conn_str))
                 else:
                     ret, frame = capturer.retrieve()
@@ -43,12 +79,44 @@ class Capturer:
                 capturer = cv.VideoCapture(self.conn_str)
                 closed = not capturer.isOpened()
                 if closed:
-                    # TODO: log error
-                    pass
+                    logging.error(
+                        f'Reconnection of the camera {self.conn_str} failed.', exc_info=True)
                 last_reconnection_try = time.time()
+
+    def stop(self):
+        self.stop_event.set()
 
 
 class MultiCapturer:
+    """
+    A class used to read multiple rtsp stream synchronously.
+
+    ...
+
+    Attributes
+    ----------
+    MAX_FAILURES_TO_SHUTDOWN_CAPTURER : int
+        the maximum number of failures to read before try to reconnect
+    frame_rate : int
+        the frame retrieval rate
+    capturers : list
+        the list of multicapturer.Capturer
+    thread : threading.Thread
+        the multi capturer thread
+    retrieve_event : threading.Event
+        the event that triggers the camera reading
+    stop_event: threading.Event
+        the event that stop the multi capturer thread execution
+    queue: queue.Queue
+        the queue that stores the batch of frames
+
+    Methods
+    -------
+    target()
+        The multi capturer's thread target function.
+    stop()
+        Stops the multi capturer's thread.
+    """
     MAX_FAILURES_TO_SHUTDOWN_CAPTURER = 30
 
     def __init__(self, conn_strs: List, frame_rate: int) -> None:
@@ -56,7 +124,7 @@ class MultiCapturer:
         self.frame_rate = frame_rate
         self.capturers = [Capturer(
             conn_str=c, max_failures_to_shutdown=MultiCapturer.MAX_FAILURES_TO_SHUTDOWN_CAPTURER*frame_rate) for c in conn_strs]
-        self.thread = threading.Thread(target=self.target)
+        self.thread = threading.Thread(target=self.target, daemon=True)
         self.stop_event = threading.Event()
         self.queue = queue.Queue(maxsize=10)
         self.thread.start()
@@ -72,19 +140,32 @@ class MultiCapturer:
                         capturer.retrieve_event.set()
                     frames = []
                     for capturer in self.capturers:
-                        capturer.done_event.wait(timeout=period)
-                        frames.append(
-                            capturer.queue.get_nowait(timeout=period))
+                        try:
+                            frames.append(
+                                capturer.queue.get(timeout=0.9*period))
+                        except queue.Empty:
+                            logging.error(
+                                f'Failed to read camera {capturer.conn_str}. Empty Buffer.', exc_info=True)
+
                         capturer.done_event.clear()
                     self.queue.put_nowait(frames)
-                # TODO: log errors
+
+                    last = now
+                    time_remaining = period + now - time.time()
+                    if time_remaining < 0:
+                        logging.error(
+                            'The application is losing frames, please consider upgrading your hardware or lowering the frame_rate')
+                    else:
+                        time.sleep(0.95*time_remaining)
                 except queue.Full:
-                    pass
-                except queue.Empty:
-                    pass
-                except:
-                    pass
-            last = now
+                    logging.error(
+                        f'Failed to push frames to output queue. The queue is full.', exc_info=True)
+                except Exception as e:
+                    logging.critical(
+                        f'Application failed unexpectedly.', exc_info=True)
+                    raise e
 
     def stop(self):
+        for capturer in self.capturers:
+            capturer.stop()
         self.stop_event.set()
